051778154416553deb087a7cfbe176d674f0a4d9
Alessandro <alemoryta@gmail.com>
2017-07-22 13:26:08 -0300

    Inlcuindo ícones da aplicação e items no nav menu

--- app/src/main/java/br/com/moryta/myfirstapp/home/HomeActivity.java
+++ app/src/main/java/br/com/moryta/myfirstapp/home/HomeActivity.java
@@ -1,9 +1,7 @@
 package br.com.moryta.myfirstapp.home;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -30,15 +28,6 @@ public class HomeActivity extends AppCompatActivity
     private void configureNavigationDrawer() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -111,18 +100,12 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_home) {
+            // Handle the home action
+        } else if (id == R.id.nav_pets) {
+            // Handle the add pet action
+        } else if (id == R.id.nav_about_us) {
+            // Show info about us
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
